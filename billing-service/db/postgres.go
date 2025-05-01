eeb6cd8d5e688e73e749c351dd7b81de69856caa
waltertaya <waltermittybecker5@gmail.com>
2025-05-01 18:11:37 +0300

    Added: Docker Compose

--- billing-service/db/postgres.go
+++ billing-service/db/postgres.go
@@ -11,15 +11,16 @@ import (
 var DB *sqlx.DB
 
 func Connect() error {
-	dbConfig := viper.Sub("database")
+	// dbConfig := viper.Sub("database")
 
 	dsn := fmt.Sprintf(
-		"host=%s port=%s password=%s dbname=%s sslmode=%s",
-		dbConfig.GetString("host"),
-		dbConfig.GetString("port"),
-		dbConfig.GetString("password"),
-		dbConfig.GetString("dbname"),
-		dbConfig.GetString("sslmode"),
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		viper.GetString("DB_HOST"),
+		viper.GetString("DB_PORT"),
+		viper.GetString("DB_USER"),
+		viper.GetString("DB_PASSWORD"),
+		viper.GetString("DB_NAME"),
+		// dbConfig.GetString("sslmode"),
 	)
 
 	var err error
