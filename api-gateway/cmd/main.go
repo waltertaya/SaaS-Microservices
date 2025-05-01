b2496e6bb6af035d1ad92ace6017e1c392590240
waltertaya <waltermittybecker5@gmail.com>
2025-05-02 02:18:13 +0300

    Add JWT validation at the gateway layer

--- api-gateway/cmd/main.go
+++ api-gateway/cmd/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"github.com/waltertaya/saas-microservices/api-gateway/middlewares"
 )
+
+func initConfig() {
+	viper.SetConfigFile("./config/config.yaml")
+
+	viper.AutomaticEnv()
+
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file: %v", err)
+	}
+}
 
 func reverseProxy(target string) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
@@ -28,13 +44,17 @@ func reverseProxy(target string) gin.HandlerFunc {
 }
 
 func main() {
+	initConfig()
 	r := gin.Default()
 
 	// Route /api/v2/auth/* to auth-service
 	r.Any("/api/v2/auth/*proxyPath", reverseProxy("http://auth-service:8080"))
 
+	// billing (with JWT)
+	billingGroup := r.Group("/api/v2/billing")
+	billingGroup.Use(middlewares.JWTMiddleware())
 	// Route /api/v2/billing/* to billing-service
-	r.Any("/api/v2/billing/*proxyPath", reverseProxy("http://billing-service:8081"))
+	billingGroup.Any("/*proxyPath", reverseProxy("http://billing-service:8081"))
 
 	// API Gateway entrypoint
 	r.Run(":8088")
---
+++ api-gateway/middlewares/jwt_middleware.go
@@ -0,0 +1,33 @@
+// middlewares/jwt.go
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"github.com/spf13/viper"
+)
+
+func JWTMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Missing or invalid Authorization header"})
+			return
+		}
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, jwt.ErrSignatureInvalid
+			}
+			return []byte(viper.GetString("jwt.secret")), nil
+		})
+		if err != nil || !token.Valid {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			return
+		}
+		c.Next()
+	}
+}
