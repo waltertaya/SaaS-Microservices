bfeaf487c992b88417f6f609eea794c5e98a9a4f
waltertaya <waltermittybecker5@gmail.com>
2025-05-02 11:49:33 +0300

    Switched to local db and local test (not using docker) - Debugging

--- api-gateway/middlewares/jwt_middleware.go
+++ api-gateway/middlewares/jwt_middleware.go
@@ -43,7 +43,9 @@ func JWTMiddleware() gin.HandlerFunc {
 			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Access restricted to premium users"})
 			return
 		}
-		c.Set("user_id", role)
+
+		userID, _ := claims["user_id"].(string)
+		c.Set("user_id", userID)
 
 		c.Next()
 	}
